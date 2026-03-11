a32db97d8662b16aba001478e2a0751d9ec77356
Caleb-Script <caleb-script@outlook.de>
2026-03-11 21:56:34 +0100

    feat(dto): add kcRawOutput

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
-export * from "./enums/user-type.enum.js";
+
+// DTO
+export * from "./dtos/mail.dto.js";
+export * from "./dtos/kc-token.dto.js";
+
+// Enums
+export * from './enums/address-type.enum.js'
 export * from "./enums/contact-options-type.enum.js";
 export * from "./enums/gender-type.enum.js";
 export * from "./enums/interest-type.enum.js";
 export * from "./enums/marital-status-type.enum.js";
 export * from "./enums/person-status.enum.js";
-export * from "./enums/relationship-type.enum.js";
 export * from "./enums/phone-number-type.enum.js";
+export * from "./enums/relationship-type.enum.js";
 export * from "./enums/role-type.enum.js";
 export * from "./enums/status-type.enum.js";
-
-
-export * from "./dtos/mail.dto.js";
+export * from "./enums/user-type.enum.js";
