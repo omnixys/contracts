269dddf1100539e0874f2d6c0c8fbd918b0f9b2f
Caleb-Script <caleb-script@outlook.de>
2026-03-13 19:34:37 +0100

    fix(exception): update  TooManyRequestsException add optional message property

--- src/exceptions/too-many-requests.exception.ts
+++ src/exceptions/too-many-requests.exception.ts
@@ -1,10 +1,14 @@
 import { HttpException, HttpStatus } from '@nestjs/common';
+interface Props { 
+  retryAfterSeconds?: number; 
+  message?: string
+}
 
 export class TooManyRequestsException extends HttpException {
-  constructor(retryAfterSeconds = 3600) {
+  constructor({retryAfterSeconds = 3600, message = 'Too many requests'}: Props  ) {
     super(
       {
-        message: 'Too many requests',
+        message,
         retryAfter: retryAfterSeconds,
       },
       HttpStatus.TOO_MANY_REQUESTS
