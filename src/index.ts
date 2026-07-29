a5f8d1c1033b067aced5e7cb5553c68624af0a0c
omnixys-01 <133212199+Caleb-Script@users.noreply.github.com>
2026-07-29 15:29:06 +0200

    fix(contracts): avoid duplicate exception exports

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,4 @@ export * from "./enums/index.js";
 export * from "./types/index.js";
 export * from "./schemas/index.js";
 export * from "./errors/index.js";
-export * from "./exceptions/index.js";
 export * from "./utils/index.js";
